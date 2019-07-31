faa4007ef56dabeb4f11fabce547f79f5cb2e781
Zane Adickes <zaneadix@gmail.com>
2019-07-30 22:52:44 -0400

    derp

--- src/templates/episode.js
+++ src/templates/episode.js
@@ -1,47 +1,47 @@
-import React from "react"
-import { Link, graphql } from "gatsby"
+// import React from "react"
+// import { Link, graphql } from "gatsby"
 
-import ConsumerContext from "../components/application-context"
-import SEO from "../components/seo"
+// import ConsumerContext from "../components/application-context"
+// import SEO from "../components/seo"
 
-export default props => {
-  let {
-    data: {
-      markdownRemark: { data },
-    },
-  } = props
+// export default props => {
+//   let {
+//     data: {
+//       markdownRemark: { data },
+//     },
+//   } = props
 
-  return (
-    <ConsumerContext>
-      {({ context, set }) => {
-        if (context.banner !== "brand") {
-          set({ banner: "brand" })
-        }
-        return (
-          <div>
-            <SEO title="About" keywords={[`gatsby`, `application`, `react`]} />
-            <h1>{data.title}</h1>
-            <Link to="">Home</Link>
-          </div>
-        )
-      }}
-    </ConsumerContext>
-  )
-}
+//   return (
+//     <ConsumerContext>
+//       {({ context, set }) => {
+//         if (context.banner !== "brand") {
+//           set({ banner: "brand" })
+//         }
+//         return (
+//           <div>
+//             <SEO title="About" keywords={[`gatsby`, `application`, `react`]} />
+//             <h1>{data.title}</h1>
+//             <Link to="">Home</Link>
+//           </div>
+//         )
+//       }}
+//     </ConsumerContext>
+//   )
+// }
 
-export const query = graphql`
-  query($slug: String!) {
-    markdownRemark(fields: { slug: { eq: $slug } }) {
-      html
-      data: frontmatter {
-        title
-        image
-        pubDate
-        image_external
-        description
-        file_location
-        image_url
-      }
-    }
-  }
-`
+// export const query = graphql`
+//   query($slug: String!) {
+//     markdownRemark(fields: { slug: { eq: $slug } }) {
+//       html
+//       data: frontmatter {
+//         title
+//         image
+//         pubDate
+//         image_external
+//         description
+//         file_location
+//         image_url
+//       }
+//     }
+//   }
+// `
